0e3dae4bd9656a4b0c7c4a49a07fb3679f3ef960
Jonas Vanderhaegen <jonas@xve.be>
2025-08-01 09:51:08 +0200

    Adjust TestCase.php

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,36 +4,4 @@ declare(strict_types=1);
 
 namespace Skylence\Shardwise\Tests;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
-use Orchestra\Testbench\TestCase as Orchestra;
 use Skylence\Shardwise\ShardwiseServiceProvider;
-
-final class TestCase extends Orchestra
-{
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'Skylence\\Shardwise\\Database\\Factories\\'.class_basename($modelName).'Factory'
-        );
-    }
-
-    public function getEnvironmentSetUp($app)
-    {
-        config()->set('database.default', 'testing');
-
-        /*
-         foreach (\Illuminate\Support\Facades\File::allFiles(__DIR__ . '/database/migrations') as $migration) {
-            (include $migration->getRealPath())->up();
-         }
-         */
-    }
-
-    protected function getPackageProviders($app)
-    {
-        return [
-            ShardwiseServiceProvider::class,
-        ];
-    }
-}
